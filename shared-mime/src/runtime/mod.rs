4d3cf1799315a8f3b4d5ec7ec750cb6fe7af3f72
Michael Ekstrand <md@ekstrandom.net>
2024-07-07 21:57:17 -0400

    clean up public types

--- shared-mime/src/runtime/mod.rs
+++ shared-mime/src/runtime/mod.rs
@@ -1,6 +1,7 @@
+//! Support for loading XDG data files at runtime or for compilation.
 mod dirs;
 pub mod xdg_mimedef;
-pub mod xdg_parse;
+mod xdg_parse;
 
 pub use dirs::xdg_mime_search_dirs;
 pub use xdg_parse::parse_mime_package;
